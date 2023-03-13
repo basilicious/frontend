824b3efd0fcd005c441d450c72ac97d5adbcdca0
Basil Labib <basillabib01@gmail.com>
2023-03-13 17:18:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from './pages/login';
+import Home from './pages/home';
+import ManageKeys from './pages/manageKeys';
+import Signup from './pages/signup';
+
+import "./custom.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/login' element={<Login />} />
+    <Route path='/' element={<Home />} />
+    <Route path='/keys' element={<ManageKeys />} />
+    <Route path='/signup' element={<Signup />} />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
---
+++ src/pages/manageKeys.js
@@ -0,0 +1,7 @@
+export default function ManageKeys() {
+    return (
+        <>
+        <div>Manage keys</div>
+        </>
+    )
+}
